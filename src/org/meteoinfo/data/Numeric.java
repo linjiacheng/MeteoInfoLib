1c6beaba0367605addfede64df9f6cf6baa71d76
wyq <wyq@yaqiang-nb>
2014-12-28 11:45:45 +0800

    run jython script

---
+++ src/org/meteoinfo/data/Numeric.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.meteoinfo.data;
+
+import org.meteoinfo.numeric.JNumeric;
+
+/**
+ *
+ * @author wyq
+ */
+public class Numeric extends JNumeric{
+    
+}
