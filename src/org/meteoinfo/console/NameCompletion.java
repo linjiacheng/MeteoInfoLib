9984ecf2205bcb1bf4a36f6f3971d11fa9c0ec35
yaqiang <yaqiang.wang@gmail.com>
2015-05-05 15:31:03 +0800

    add console package

---
+++ src/org/meteoinfo/console/NameCompletion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.meteoinfo.console;
+
+/**
+ *
+ * @author yaqiang
+ */
+public abstract interface NameCompletion {
+    public abstract String[] completeName(String paramString);
+}
