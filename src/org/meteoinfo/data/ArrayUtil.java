ad0078876332f41ad1aab0f9b1d891f0e85eb1ea
wyq <wyq@DESKTOP-wyq>
2017-04-25 12:59:59 +0800

    update project function

--- src/org/meteoinfo/data/ArrayUtil.java
+++ src/org/meteoinfo/data/ArrayUtil.java
@@ -3246,7 +3246,7 @@ public class ArrayUtil {
      * @return Porjected grid data
      * @throws ucar.ma2.InvalidRangeException
      */
-    public static Object[] reproject(Array data, List<Number> xx, List<Number> yy, ProjectionInfo fromProj, ProjectionInfo toProj) throws InvalidRangeException {
+    public static Object[] reproject_back(Array data, List<Number> xx, List<Number> yy, ProjectionInfo fromProj, ProjectionInfo toProj) throws InvalidRangeException {
         Extent aExtent;
         int xnum = xx.size();
         int ynum = yy.size();
@@ -3304,6 +3304,61 @@ public class ArrayUtil {
         }
 
         return new Object[]{r, rx, ry};
+    }
+    
+    /**
+     * Project grid data
+     *
+     * @param data Data array
+     * @param xx X array
+     * @param yy Y array
+     * @param fromProj From projection
+     * @param toProj To projection
+     * @param method Resample method
+     * @return Porjected grid data
+     * @throws ucar.ma2.InvalidRangeException
+     */
+    public static Object[] reproject(Array data, List<Number> xx, List<Number> yy, ProjectionInfo fromProj, 
+            ProjectionInfo toProj, ResampleMethods method) throws InvalidRangeException {
+        Extent aExtent;
+        int xnum = xx.size();
+        int ynum = yy.size();
+        aExtent = ProjectionManage.getProjectionExtent(fromProj, toProj, xx, yy);
+
+        double xDelt = (aExtent.maxX - aExtent.minX) / (xnum - 1);
+        double yDelt = (aExtent.maxY - aExtent.minY) / (ynum - 1);        
+        int i;   
+        Array rx = Array.factory(DataType.DOUBLE, new int[]{xnum});
+        Array ry = Array.factory(DataType.DOUBLE, new int[]{ynum});
+        for (i = 0; i < xnum; i++) {
+            rx.setDouble(i, aExtent.minX + i * xDelt);
+        }
+
+        for (i = 0; i < ynum; i++) {
+            ry.setDouble(i, aExtent.minY + i * yDelt);
+        }
+        
+        Array[] rr = ArrayUtil.meshgrid(rx, ry);        
+                
+        Array r = ArrayUtil.reproject(data, xx, yy, rr[0], rr[1], fromProj, toProj, method);
+
+        return new Object[]{r, rx, ry};
+    }
+    
+    /**
+     * Project grid data
+     *
+     * @param data Data array
+     * @param xx X array
+     * @param yy Y array
+     * @param fromProj From projection
+     * @param toProj To projection
+     * @return Porjected grid data
+     * @throws ucar.ma2.InvalidRangeException
+     */
+    public static Object[] reproject(Array data, List<Number> xx, List<Number> yy, ProjectionInfo fromProj, 
+            ProjectionInfo toProj) throws InvalidRangeException {
+        return reproject(data, xx, yy, fromProj, toProj, ResampleMethods.NearestNeighbor);
     }
 
     /**
